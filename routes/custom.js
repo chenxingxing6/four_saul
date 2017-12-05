46d93a6bd6b134f1e213ebd305afed674b340e76
xiaobo <1968468227@qq.com>
2017-12-05 20:50:14 +0800

    增加周边客户，周边商户，营销客户，营销商户

--- routes/custom.js
+++ routes/custom.js
@@ -6,15 +6,109 @@ var config = require('../config/config');
 /* GET home page. */
 router.get('/', function(req, res, next) {
     res.render('custom', {
-        title: 'Express'
+        data: data
     });
 });
 
-router.post('/search', function(req, res, next) {
+router.get('/person', function(req, res, next) {
+    var data = [{
+        name: '小波',
+        personId: '12',
+        exsit_service: '信用贷，影视贷，保险',
+        address: '江西省南昌市经济开发区华东交通大学北区35栋302'
+    }, {
+        name: '小波',
+        personId: '12',
+        exsit_service: '信用贷，影视贷，保险',
+        address: '江西省南昌市经济开发区华东交通大学北区35栋302'
+    }, {
+        name: '小波',
+        personId: '12',
+        exsit_service: '信用贷，影视贷，保险',
+        address: '江西省南昌市经济开发区华东交通大学北区35栋302'
+    }, {
+        name: '小波',
+        personId: '12',
+        exsit_service: '信用贷，影视贷，保险',
+        address: '江西省南昌市经济开发区华东交通大学北区35栋302'
+    }, {
+        name: '小波',
+        personId: '12',
+        exsit_service: '信用贷，影视贷，保险',
+        address: '江西省南昌市经济开发区华东交通大学北区35栋302'
+    }];
+    res.render('custom/person', {
+        data: data
+    });
+});
+
+router.post('/person/search', function(req, res, next) {
     var form = req.body;
     console.log(form);
-    res.render('custom', {
-        title: 'Express'
+    var data = [{
+        name: '小波',
+        personId: '12',
+        exsit_service: '信用贷，影视贷，保险',
+        address: '江西省南昌市经济开发区华东交通大学北区35栋302'
+    }, {
+        name: '小波',
+        personId: '12',
+        exsit_service: '信用贷，影视贷，保险',
+        address: '江西省南昌市经济开发区华东交通大学北区35栋302'
+    }];
+    res.render('custom/person', {
+        data: data
+    });
+});
+
+router.get('/shop', function(req, res, next) {
+    var data = [{
+        name: 'vans专卖店',
+        shopId: '12',
+        exsit_service: '信用贷，影视贷，保险',
+        address: '江西省南昌市经济开发区华东交通大学北区35栋302'
+    }, {
+        name: 'vans专卖店',
+        shopId: '12',
+        exsit_service: '信用贷，影视贷，保险',
+        address: '江西省南昌市经济开发区华东交通大学北区35栋302'
+    }, {
+        name: 'vans专卖店',
+        shopId: '12',
+        exsit_service: '信用贷，影视贷，保险',
+        address: '江西省南昌市经济开发区华东交通大学北区35栋302'
+    }, {
+        name: 'vans专卖店',
+        shopId: '12',
+        exsit_service: '信用贷，影视贷，保险',
+        address: '江西省南昌市经济开发区华东交通大学北区35栋302'
+    }, {
+        name: 'vans专卖店',
+        shopId: '12',
+        exsit_service: '信用贷，影视贷，保险',
+        address: '江西省南昌市经济开发区华东交通大学北区35栋302'
+    }];
+    res.render('custom/shop', {
+        data: data
+    });
+});
+
+router.post('/shop/search', function(req, res, next) {
+    var form = req.body;
+    console.log(form);
+    var data = [{
+        name: 'vans专卖店',
+        shopId: '12',
+        exsit_service: '信用贷，影视贷，保险',
+        address: '江西省南昌市经济开发区华东交通大学北区35栋302'
+    }, {
+        name: 'vans专卖店',
+        shopId: '12',
+        exsit_service: '信用贷，影视贷，保险',
+        address: '江西省南昌市经济开发区华东交通大学北区35栋302'
+    }];
+    res.render('custom/shop', {
+        data: data
     });
 });
 
