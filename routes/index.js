3cf4a646127ad78afc3cba9240f8fadd833e9a35
xiaobo <1968468227@qq.com>
2017-12-18 16:21:25 +0800

    修改照片

--- routes/index.js
+++ routes/index.js
@@ -2,11 +2,18 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/wechat', function(req, res, next) {
+    if (req.query.type == 0) {
+        var type = 'member';
+    } else if (req.query.type == 1) {
+        var type = 'leader';
+    } else {
+        var type = 'boss';
+    }
     res.cookie('openid', req.query.openid);
     res.cookie('name', req.query.name);
     res.cookie('staffId', req.query.staffid);
     res.cookie('id', req.query.id);
-    res.cookie('type', req.query.type);
+    res.cookie('type', type);
     res.cookie('headPic', req.query.headPic);
     console.log('wechat');
     console.log(req.query);
