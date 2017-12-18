262ab857b282854f7b2777071b7d1518639495de
xiaobo <1968468227@qq.com>
2017-12-18 16:13:26 +0800

    线上调试

--- routes/index.js
+++ routes/index.js
@@ -8,20 +8,22 @@ router.get('/wechat', function(req, res, next) {
     res.cookie('id', req.query.id);
     res.cookie('type', req.query.type);
     res.cookie('headPic', req.query.headPic);
-    // console.log(req.query);
-    if(req.query.id){
-    	res.redirect('/');
-    }else{
-    	res.redirect('/login');
+    console.log('wechat');
+    console.log(req.query);
+    if (req.query.id) {
+        res.redirect('/');
+    } else {
+        res.redirect('/login');
     }
 });
 
 router.get('/', function(req, res, next) {
-    // console.log(req.cookies);
-    if(!req.cookies.openid){
-    	res.redirect('/wechat');
-    }else{
-    	res.render('index', { data: req.cookies });
+    console.log('login')
+    console.log(req.cookies);
+    if (!req.cookies.openid) {
+        res.redirect('/wechat');
+    } else {
+        res.render('index', { data: req.cookies });
     }
 });
 
